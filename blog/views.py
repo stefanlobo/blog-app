bce89949b91d872ea65d0301f0c4ceae4f92bb4b
slobo <stefanlobo12@gmail.com>
2025-06-27 15:59:32 -0700

    create basic views for post and its details

--- blog/views.py
+++ blog/views.py
@@ -11,10 +11,13 @@ def post_list(request):
 
 
 def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("No Post found.")
+
+    post = get_object_or_404(
+        Post,
+        id=id,
+        status=Post.Status.PUBLISHED,
+    )
+
     return render(
         request,
         "blog/post/detail.html",
