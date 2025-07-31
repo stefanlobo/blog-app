c9fddc36522aad6a9f8bb703f077033d1c97ed48
slobo <stefan.lobo@intel.com>
2025-07-31 16:24:28 -0700

    add comments comment html

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from django.http import Http404
 from django.views.generic import ListView
 from .models import Post
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 # Create your views here.
 
@@ -50,10 +51,18 @@ def post_detail(request, year, month, day, post):
         publish__day=day,
     )
 
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    # From for users to comment
+    form = CommentForm()
+
+
     return render(
         request,
         "blog/post/detail.html",
-        {"post": post},
+        {"post": post,
+         "comments": comments,
+         "form": form},
     )
 
 
@@ -96,4 +105,30 @@ def post_share(request, post_id):
             "form": form,
             "sent": sent,
         },
+    )
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id,
+                             status=Post.status.PUBLISHED)
+    comment = None
+    # A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a Comment object but don't save to database yet
+        comment = form.save(commit=False)
+        # Assign the current post to the comment
+        comment.post = post
+
+        # Save the comment to the database
+        comment.save()
+    
+    return render(
+        request, 
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment,
+        }
     )
