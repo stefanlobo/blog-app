cee1c59db55f35a4fe39c66a77b6d13a6e126f5e
slobo <stefan.lobo@intel.com>
2025-08-06 12:09:18 -0700

    add search func add weighted search add trigram ext to db

--- blog/views.py
+++ blog/views.py
@@ -6,9 +6,10 @@ from django.http import Http404
 from django.db.models import Count
 from django.views.generic import ListView
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchRank, SearchQuery, TrigramSimilarity
 from taggit.models import Tag
 from .models import Post
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
 
 # Create your views here.
@@ -159,5 +160,38 @@ def post_comment(request, post_id):
             "post": post,
             "form": form,
             "comment": comment,
+        },
+    )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            search_vector = SearchVector(
+                "title", weight='A'
+            ) + SearchVector("body", weight='B')
+            search_query = SearchQuery(query)
+            results = (
+                Post.published.annotate(
+                    search=search_vector,
+                    rank=SearchRank(search_vector, search_query),
+                )
+                .filter(rank__gte=0.3)
+                .order_by("-rank")
+            )
+
+    return render(
+        request,
+        "blog/post/search.html",
+        {
+            "form": form,
+            "query": query,
+            "results": results,
         },
     )
