46e58463b357c440de635aa4c34a38fd88d08527
slobo <stefan.lobo@intel.com>
2025-08-06 12:12:49 -0700

    add search by trigram

--- blog/views.py
+++ blog/views.py
@@ -173,17 +173,16 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data["query"]
-            search_vector = SearchVector(
-                "title", weight='A'
-            ) + SearchVector("body", weight='B')
-            search_query = SearchQuery(query)
+            # search_vector = SearchVector(
+            #     "title", weight='A'
+            # ) + SearchVector("body", weight='B')
+            # search_query = SearchQuery(query)
             results = (
                 Post.published.annotate(
-                    search=search_vector,
-                    rank=SearchRank(search_vector, search_query),
+                    similarity=TrigramSimilarity('title', query)
                 )
-                .filter(rank__gte=0.3)
-                .order_by("-rank")
+                .filter(similarity__gte=0.1)
+                .order_by("-similarity")
             )
 
     return render(
